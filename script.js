4116cc7ded60e2cb6b79f193304810078092866d
Glen Alloy <glenalloy8@gmail.com>
2024-05-15 08:03:27 +0530

    add decimal done

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ function sendNumberValue(number){
     const displayValue = calculatorDisplay.textContent;
     calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
-
+function addDecimal(){
+    if(!calculatorDisplay.textContent.includes('.')){
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
 inputBtns.forEach((inputBtn) =>{
     if(inputBtn.classList.length === 0){
         inputBtn.addEventListener('click',() => sendNumberValue(inputBtn.value));
     }else if(inputBtn.classList.contains('operator')){
         inputBtn.addEventListener('click',() => sendNumberValue(inputBtn.value));
     }else if(inputBtn.classList.contains('decimal')){
-        inputBtn.addEventListener('click',() =>sendNumberValue());
+        inputBtn.addEventListener('click',() => addDecimal());
     }
 });
 
