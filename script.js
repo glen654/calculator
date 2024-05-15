98bdf0251a658f4130e142b07c9eb5c1506ad680
Glen Alloy <glenalloy8@gmail.com>
2024-05-15 08:16:02 +0530

    using operators done

--- script.js
+++ script.js
@@ -7,25 +7,46 @@ let operatorValue = '';
 let awaitingNextValue = false;
 
 function sendNumberValue(number){
-    const displayValue = calculatorDisplay.textContent;
-    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    if(awaitingNextValue){
+        calculatorDisplay.textContent = number;
+        awaitingNextValue = false;
+    }else {
+        const displayValue = calculatorDisplay.textContent;
+        calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    }
 }
 function addDecimal(){
     if(!calculatorDisplay.textContent.includes('.')){
         calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
     }
 }
+
+function useOperator(operator){
+    const currentValue = Number(calculatorDisplay.textContent);
+    if(!firstValue){
+        firstValue = currentValue;
+    }else{
+        console.log('currentValue',currentValue);
+    }
+    awaitingNextValue = true;
+    operatorValue = operator;
+    console.log('firstValue', firstValue);
+    console.log('operator', operatorValue);
+}
 inputBtns.forEach((inputBtn) =>{
     if(inputBtn.classList.length === 0){
         inputBtn.addEventListener('click',() => sendNumberValue(inputBtn.value));
     }else if(inputBtn.classList.contains('operator')){
-        inputBtn.addEventListener('click',() => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click',() => useOperator(inputBtn.value));
     }else if(inputBtn.classList.contains('decimal')){
         inputBtn.addEventListener('click',() => addDecimal());
     }
 });
 
 function resetAll(){
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
     calculatorDisplay.textContent = '0';
 }
 
