5ba2187f31cf785e40af9355eef5ea6c8f238a18
Glen Alloy <glenalloy8@gmail.com>
2024-05-15 08:06:05 +0530

    three new globla variables created

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
 function sendNumberValue(number){
     const displayValue = calculatorDisplay.textContent;
     calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
